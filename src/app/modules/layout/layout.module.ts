7936711178b9c12f237c1753429d65630d840cb9
somasekhar233 <somasekhar@vassarlabs.com>
2021-12-15 22:21:40 +0530

    UI::Additions

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbDropdown, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
     CommonModule,
     FormsModule,
+    NgbDropdownModule,
     ReactiveFormsModule,
     RouterModule.forChild([])
   ],
